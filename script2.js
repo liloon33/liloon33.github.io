aeeed352f914ef485e55ef9d2c5bf9019fc384b9
liloon33 <100419158+liloon33@users.noreply.github.com>
2022-04-16 22:18:10 +0800

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,18 @@
+
+
+function sayHello(){
+  var response = prompt("What's your name?");
+  alert("Hello " +  response + "  , have a nice day!  :) ");
+}
+sayHello();
+
+function giveHints(){
+  alert("Well,don't take anything for granted")
+}
+
+function reportError(){
+  alert("error")
+}
+function showAnswer(){
+  alert("The answers are all B!")
+}
